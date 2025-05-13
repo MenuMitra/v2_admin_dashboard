1b6d1551dea2fef688c58f3883c0d217c605819d
Santosh Mhetre <santoshmhetre602@gmail.com>
2025-05-13 17:28:43 +0530

    sa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/AdminDashboard' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/AdminDashboard/' : '',
+};
 
 export default nextConfig;
