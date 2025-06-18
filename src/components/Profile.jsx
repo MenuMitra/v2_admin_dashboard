23c954144250b7d3cf92efe2a6fe2830e7ccd99c
Sugatraj <sugatraj.2106@gmail.com>
2025-06-18 10:28:14 +0530

    Refactor Profile component: comment out unused elements for improved code clarity and maintain existing functionalities.

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -35,7 +35,7 @@ function Profile() {
   // Custom render function for the profile content
   const renderProfileContent = () => (
     <div className="p-5 dark:border-gray-800 lg:p-6">
-      <div className="p-5 mb-6 border border-gray-200 rounded-2xl dark:border-gray-800 lg:p-6">
+      {/* <div className="p-5 mb-6 border border-gray-200 rounded-2xl dark:border-gray-800 lg:p-6">
         <div className="flex flex-col gap-5 xl:flex-row xl:items-center xl:justify-between">
           <div className="flex flex-col items-center w-full gap-6 xl:flex-row">
             <div className="w-20 h-20 overflow-hidden border border-gray-200 rounded-full dark:border-gray-800">
@@ -53,10 +53,10 @@ function Profile() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
 
       {/* Personal Information Section */}
-      <div className="p-5 border border-gray-200 rounded-2xl dark:border-gray-800 lg:p-6">
+      <div className=" rounded-2xl dark:border-gray-800 lg:p-6">
         <div>
           <h4 className="text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-6">
             Personal Information
