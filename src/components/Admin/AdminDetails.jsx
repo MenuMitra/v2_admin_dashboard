bf1ba104c97d0d6f98ddb7bcec986adbfc1bdd34
Sugatraj <sugatraj.2106@gmail.com>
2025-06-19 18:08:28 +0530

    Refactor date formatting in AdminDetails component: update display format to "Day Month Year" and include AM/PM notation for improved clarity and user experience.

--- src/components/Admin/AdminDetails.jsx
+++ src/components/Admin/AdminDetails.jsx
@@ -49,9 +49,14 @@ function AdminDetails() {
     const month = months[date.getMonth()];
     const day = date.getDate().toString().padStart(2, "0");
     const year = date.getFullYear();
-    const hours = date.getHours().toString().padStart(2, "0");
+    
+    let hours = date.getHours();
     const minutes = date.getMinutes().toString().padStart(2, "0");
-    return `${month} ${day}, ${year} at ${hours}:${minutes}`;
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // convert 0 to 12
+    
+    return `${day} ${month} ${year} at ${hours}:${minutes} ${ampm}`;
   };
 
   useEffect(() => {
