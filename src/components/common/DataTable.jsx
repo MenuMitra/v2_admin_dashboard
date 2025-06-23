722e62e146f65c84608b95f08890d2b634310c50
Sugatraj <sugatraj.2106@gmail.com>
2025-06-23 14:55:21 +0530

    Update styling in DataTable component: change font weight and text color for improved readability and visual consistency in dark mode.

--- src/components/common/DataTable.jsx
+++ src/components/common/DataTable.jsx
@@ -549,7 +549,7 @@ function DataTable({
                   }
                 >
                   <div className="flex items-center justify-center">
-                    <p className={`font-medium text-gray-500 text-theme-xs ${darkMode ? "dark:text-gray-400" : ""}`}>
+                    <p className={`font-semibold text-gray-700 text-theme-xs ${darkMode ? "dark:text-white/90" : ""}`}>
                       {column.header}
                     </p>
                     {column.sortable && renderSortIcon(column.field)}
