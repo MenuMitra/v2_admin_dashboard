5a9b1d7349054da15a6dbfb96fdc9456a060793b
Sugatraj <sugatraj.2106@gmail.com>
2025-06-27 23:11:37 +0530

    Update SVG icon size in TestEnvironmentBanner component: reduce dimensions for improved visual consistency and alignment.

--- src/components/Banner/TestEnvironmentBanner.jsx
+++ src/components/Banner/TestEnvironmentBanner.jsx
@@ -22,8 +22,8 @@ const TestEnvironmentBanner = () => {
     <div className="flex justify-between items-center px-2 py-1 text-white bg-warning-500 shadow-md transition-all duration-300">
       <div className="flex-grow flex items-center justify-center text-center w-full mx-auto">
         <svg
-          width="24"
-          height="24"
+          width="18"
+          height="18"
           viewBox="0 0 24 24"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
