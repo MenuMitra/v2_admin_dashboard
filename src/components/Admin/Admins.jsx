0cc884cd92c346bb03332a6b93ce3f578b227126
Sugatraj <sugatraj.2106@gmail.com>
2025-07-14 14:31:26 +0530

    Update button styles in Admins component for improved visual consistency and user experience by changing buttons to rounded-full.

--- src/components/Admin/Admins.jsx
+++ src/components/Admin/Admins.jsx
@@ -388,14 +388,14 @@ function Admins() {
                 setShowDeleteModal(false);
                 setAdminToDelete(null);
               }}
-              className="flex justify-center rounded-lg border border-gray-300 bg-white px-4 py-3 text-theme-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200 sm:w-auto"
+              className="flex justify-center rounded-full border border-gray-300 bg-white px-4 py-3 text-theme-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200 sm:w-auto"
             >
               Cancel
             </button>
             <button
               type="button"
               onClick={handleDeleteAdmin}
-              className="flex justify-center px-4 py-3 text-theme-sm font-medium text-white rounded-lg bg-error-500 shadow-theme-xs hover:bg-error-600 sm:w-auto"
+              className="flex justify-center px-4 py-3 text-theme-sm font-medium text-white rounded-full bg-error-500 shadow-theme-xs hover:bg-error-600 sm:w-auto"
             >
               Delete
             </button>
