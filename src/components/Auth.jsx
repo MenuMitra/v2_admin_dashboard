dbcf631d82c49ee07015f041d14e283dcd7365d6
Sugatraj <sugatraj.2106@gmail.com>
2025-07-14 13:45:48 +0530

    Add introductory video to Auth component with improved styling for better user engagement and layout consistency.

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -322,6 +322,31 @@ function Auth() {
               {isOtpSent && (
                 <div>
                   <div className="mb-5 sm:mb-8">
+                  <div className="pb-10 size-full">
+                    {/* <iframe
+                      src="https://www.youtube.com/embed/j2e2stCcICo?autoplay=1&mute=1&loop=1&playlist=j2e2stCcICo"
+                      title="MenuMitra Intro Video | Restaurant POS made easy | Shekru Labs India Pvt. Ltd"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      referrerPolicy="strict-origin-when-cross-origin"
+                      allowFullScreen
+                      className="rounded-lg "
+                    /> */}
+
+                    <div class="rounded-2xl border border-gray-200 bg-white dark:border-gray-800 dark:bg-white/[0.03]">
+                      
+                        <div class="overflow-hidden rounded-lg aspect-video">
+                          <iframe
+                            src="https://www.youtube.com/embed/j2e2stCcICo?autoplay=1&mute=1&loop=1&playlist=j2e2stCcICo"
+                            title="YouTube video"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen=""
+                            class="w-full h-full"
+                          ></iframe>
+                        </div>
+                      
+                    </div>
+                  </div>
                     <h1 className="mb-2 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md">
                       Verify OTP
                     </h1>
