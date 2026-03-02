c5d03950e6f9ee5968b5c6e9bbbcb281c8179b08
ABC <j.rushikesh013@gmail.com>
2026-03-02 16:24:28 +0530

    Changes Done

---
+++ debug-menu-api.js
@@ -0,0 +1,17 @@
+
+const axios = require('axios');
+
+const BASE_URL = 'https://mitra.pizeonfly.com/api/v2.2'; // Based on common pattern in codebase
+const token = 'YOUR_TOKEN_HERE'; // I need to find a way to get a token or just assume the structure from code
+
+async function testMenuList() {
+    try {
+        // This is just a placeholder to show what I'd want to test if I had credentials
+        // Since I can't easily get a token here, I'll relies on code analysis
+        console.log("Analyzing code structure instead of direct API call due to auth requirements.");
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+testMenuList();
